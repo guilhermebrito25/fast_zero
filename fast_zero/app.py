b249a5012da8ac4b9855e7a57701f35bf96f22e1
Guilherme Brito <94586533+guilhermebrito25@users.noreply.github.com>
2024-10-05 12:30:44 -0400

    att

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -12,5 +12,10 @@ database = []
 @app.post('/users', status_code=HTTPStatus.CREATED, response_model=UserPublic)
 def create_user(user: UserSchema):
     user_with_id = UserDB(id=len(database) + 1, **user.model_dump())
-
+    database.append(user_with_id)
     return user_with_id
+
+
+@app.get('/users', status_code=HTTPStatus.OK)
+def read_users():
+    return {'user': database}
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,12 +1,7 @@
 from http import HTTPStatus
 
-from fastapi.testclient import TestClient
 
-from fast_zero.app import app
-
-
-def test_create_user():
-    client = TestClient(app)
+def test_create_user(client):
     response = client.post(
         '/users',
         json={
